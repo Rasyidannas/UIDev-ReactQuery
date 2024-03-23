a7cb6103f97416f29213bf4c8bfb5e8a5b39c74e
rasyid annas <rasshit.dsgn@gmail.com>
2024-03-23 19:39:16 +0700

    Lesson 42: Optimistic Update

---
+++ src/components/IssueAssignment.jsx
@@ -0,0 +1,83 @@
+import { useUserData } from "../helpers/useUserData";
+import { GoGear } from "react-icons/go";
+import { useState } from "react";
+import { useQuery, useQueryClient, useMutation } from "react-query";
+
+export default function IssueAssignment({ assignee, issueNumber }) {
+  const user = useUserData(assignee);
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const usersQuery = useQuery(["users"], () =>
+    fetch("/api/users").then((res) => res.json())
+  );
+
+  const queryClient = useQueryClient();
+
+  const setAssignment = useMutation(
+    (assignee) => {
+      fetch(`/api/issues/${issueNumber}`, {
+        method: "PUT",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({ assignee }),
+      }).then((res) => res.json());
+    },
+    {
+      onMutate: (assignee) => {
+        const oldAssignee = queryClient.getQueryData([
+          "issues",
+          issueNumber,
+        ]).assignee;
+
+        //this for optimistic update data
+        queryClient.setQueryData(["issues", issueNumber], (data) => ({
+          ...data,
+          assignee,
+        }));
+
+        //this for error/fail update
+        return function rollback() {
+          queryClient.setQueryData(["issues", issueNumber], (data) => ({
+            ...data,
+            assignee: oldAssignee,
+          }));
+        };
+      },
+      onError: (error, variables, rollback) => {
+        rollback();
+      },
+      onSettled: () => {
+        //onSettled is for finish ignore success or error
+        queryClient.invalidateQueries(["issues", issueNumber], { exact: true });
+      },
+    }
+  );
+
+  return (
+    <div className="issue-options">
+      <div>
+        <span>Assignment</span>
+        {user.isSuccess && (
+          <div>
+            <img src={user.data.profilePictureUrl} />
+            {user.data.name}
+          </div>
+        )}
+      </div>
+      <GoGear
+        onClick={() => !usersQuery.isLoading && setMenuOpen((open) => !open)}
+      />
+      {menuOpen && (
+        <div className="picker-menu">
+          {usersQuery.data?.map((user) => (
+            <div key={user.id} onClick={() => setAssignment.mutate(user.id)}>
+              <img src={user.profilePictureUrl} />
+              {user.name}
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
